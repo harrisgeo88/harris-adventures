24e417abed2542e3f91d9301b8ad0eec53b8f124
Harris Geo <harris@veoo.com>
2016-12-28 20:11:45 +0200

    added first component and code cleanup

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,10 @@
-alert('yo');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var FirstComponent = React.createClass({
+  render() {
+    return <h1>Hey Harris</h1>
+  }
+});
+
+ReactDOM.render(<FirstComponent/>, document.getElementById('first-component'));
