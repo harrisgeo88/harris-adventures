7803a5d4da9138a0126c7849ec13b9392cf836d1
Harris Geo <harris@veoo.com>
2016-12-29 19:15:58 +0200

    added new page suggestions

--- src/app/router.js
+++ src/app/router.js
@@ -1,11 +1,13 @@
 var {Router, Route, browserHistory} = require('react-router');
 var MainComponent = require('./main');
 var React = require('react');
+var Suggestions = require('./suggestions');
 
 var App = React.createClass({
   render() {
     return <Router history={browserHistory}>
       <Route path="/" component={MainComponent}></Route>
+      <Route path="/suggestions" component={Suggestions}/>
     </Router>
   }
 })
---
+++ src/app/suggestions.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var FormInput = require('../components/form_input');
+var BS = require('react-bootstrap');
+
+var Test = React.createClass({
+  render() {
+    // console.log(yo)
+    return <form id="suggestions-form" style={{width:700, margin: 'auto'}}>
+      <h2>There are always new places to discover</h2>
+      <h2><small>If you have any to recommend let me know</small></h2>
+      <FormInput placeholder="country or destination name" type="text"/>
+      <FormInput placeholder="any tips/suggestions" type="textarea"/>
+      <BS.Button type="submit" bsStyle="primary">Send Suggestion</BS.Button>
+    </form>
+  }
+});
+
+module.exports = Test;
